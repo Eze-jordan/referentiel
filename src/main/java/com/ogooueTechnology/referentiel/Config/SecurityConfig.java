93ddc9b91f9559607ae2f8056904b8f9bfeae19c
unknown <Eze-jordan>
2025-07-30 22:47:32 +0100

    cors

--- src/main/java/com/ogooueTechnology/referentiel/Config/SecurityConfig.java
+++ src/main/java/com/ogooueTechnology/referentiel/Config/SecurityConfig.java
@@ -64,13 +64,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // ← frontend autorisé
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/", configuration);
         return source;
     }
 
