0c04d595bc4e6728f0b0f121c5fc0e9fe4606a3c
unknown <Eze-jordan>
2026-02-24 23:29:19 +0100

    WebConfig

---
+++ src/main/java/com/ogooueTechnology/referentiel/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.ogooueTechnology.referentiel.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuditInterceptor auditInterceptor;
+
+    public WebConfig(AuditInterceptor auditInterceptor) {
+        this.auditInterceptor = auditInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(auditInterceptor)
+                .addPathPatterns("/api/**") // intercepte toutes les routes API
+                .excludePathPatterns("/api/V1/audit-logs/**"); // évite boucle infinie
+    }
+}
